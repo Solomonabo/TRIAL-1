ed55857d8642455e2e9b0750b328440db0be260d
Solomonabo <46652792+Solomonabo@users.noreply.github.com>
2019-02-16 21:23:20 -0800

    update gallery html

---
+++ test.js
@@ -0,0 +1,31 @@
+//.this.is.asingle-line.JavaScript.comment
+
+/*
+this.is.a.multi-line.
+*/
+//This is how arithmetic operations works//
+
+8*9
+12/4
+14%6{functions as a remainder}
+5+8
+8-6
+++5
+5++
+--4
+4--
+//Types of data in javascript//
+Booleen {True or false}
+Numbers
+Symbols
+Strings {words}
+objects
+Null
+undefined 
+//Strings concantenation//
+adding or bringing series of strings together 
+const originalName=('Osakpakpa');
+console.log(`My parent gave me ${originalName} when I was born`);
+console.log("originalName" +" 32");
+console.log(typeof originalName);
+
